fda3ba37c7a0a41748c137afb429fc384478652e
Petyo Lazarov <plazarov92@gmail.com>
2017-06-14 22:47:23 +0300

    Task 4-1 Notifications

--- scripts/prodavachnik.js
+++ scripts/prodavachnik.js
@@ -1,4 +1,10 @@
 function startApp() {
+
+    if (sessionStorage.getItem('authToken') !== null) {
+        let username = sessionStorage.getItem('username');
+        $('#loggedInUser').text("Welcome, " + username + "!");
+    }
+
     showHideMenuLinks();
     showHomeView();
 
@@ -8,6 +14,7 @@ function startApp() {
     $("#linkRegister").click(showRegisterView);
     $("#linkListAds").click(listAdverts);
     $("#linkLogout").click(logoutUser);
+
 
     // Bind the form submit buttons
     $("#buttonLoginUser").click(loginUser);
@@ -31,12 +38,14 @@ function startApp() {
             $("#linkRegister").show();
             $("#linkListAds").hide();
             $("#linkLogout").hide();
+            $("#loggedInUser").hide();
         } else {
             // We have logged in user
             $("#linkLogin").hide();
             $("#linkRegister").hide();
             $("#linkListAds").show();
             $("#linkLogout").show();
+            $("#loggedInUser").show();
         }
     }
 
@@ -85,6 +94,9 @@ function startApp() {
         sessionStorage.setItem('authToken', userAuth);
         let userId = userInfo._id;
         sessionStorage.setItem('userId', userId);
+        let username = userInfo.username;
+        sessionStorage.setItem('username', username);
+        $('#loggedInUser').text("Welcome, " + username + "!");
     }
 
     // user/register
